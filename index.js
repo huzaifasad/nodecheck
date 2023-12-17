f1d0e122ec9582bee3b9c3a23a5b2b07ead6be8f
huzaifasad <huzaifa084567@gmail.com>
2023-12-17 09:01:41 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("./schemas/config");
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(1000, () => {
+  console.log("Server is running on port 1000");
+});
+
+app.use(
+  cors({
+    origin: ["https://backendbazar-pk.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+const laptoprouter = require("./schemas/productrouter/laptop");
+const userrouter = require("./schemas/productrouter/user");
+app.use("/laptop", laptoprouter);
+app.use("/user", userrouter);
+app.use("/", (req, res) => {
+  res.send("thiis response");
+});
